8beeaf09601b8a789479c78edaf8e5eb8035b177
PremFTW <32745662+PremFTW@users.noreply.github.com>
2018-02-15 13:53:10 -0500

    Add files via upload

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,16 +1,33 @@
-def shift(letter, n):
-    pass
+
+def shift(letter, shift_amount):
+    
+    if letter == [ "|", "}", "~"]:
+        return letter
+    
+    else:
+        unicode = ord(letter) + shift_amount
+        new_letter = chr(unicode)
+
+    return new_letter
 
 def encrypt(message, shift_amount):
-    pass
+    result = ""
+
+    for letter in message:
+        result += shift(letter, shift_amount)
+
+    return result
 
 
 def decrypt(message, shift_amount):
-    pass
 
-secret_message = "encryption is fun"
+    return encrypt(message, -1*shift_amount)
+
+secret_message = " PremFTW SASASKJHALKFHALIiowhlddsjkhdf12390='=Q12''/* "
 encrypted_message = encrypt(secret_message, 3)
+decrypted_message = decrypt(encrypted_message, 3)
 
+print(secret_message)
 print(encrypted_message)
-
+print(decrypted_message)
 
